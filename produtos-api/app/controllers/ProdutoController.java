bd4ec79e2e8286145b9890fad4705a66c06113ee
hemersonGH <hemersonel@gmail.com>
2018-10-28 12:07:39 -0300

    Adicionando views, routes e  config ao bd

---
+++ produtos-api/app/controllers/ProdutoController.java
@@ -0,0 +1,26 @@
+package controllers;
+
+import javax.inject.Inject;
+
+import models.Produto;
+import play.data.Form;
+import play.data.FormFactory;
+import play.mvc.*;
+import views.html.*;
+
+public class ProdutoController extends Controller {
+
+	@Inject
+	private FormFactory formularios;
+
+	public Result salvaNovoProduto() {
+		Form<Produto> formulario = formularios.form(Produto.class).bindFromRequest();
+		Produto produto = formulario.get();
+		produto.save();
+		return redirect(routes.ProdutoController.formularioDeNovoProduto());
+	}
+
+	public Result formularioDeNovoProduto() {
+		return ok(formularioDeNovoProduto.render("Cadastrar produto"));
+	}
+}
